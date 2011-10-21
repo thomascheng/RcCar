fde93a52643aa5a2fcf5417c2151fc56e4a75eb6
Jessie Young <mewsicalcat@gmail.com>
2011-10-21 00:55:42 -0400

    commit with dave's rc car stuff

--- MasterController/src/edu/jhu/cs/oose/group2/MasterControllerActivity.java
+++ MasterController/src/edu/jhu/cs/oose/group2/MasterControllerActivity.java
@@ -3,6 +3,7 @@ package edu.jhu.cs.oose.group2;
 import android.app.Activity;
 import android.os.Bundle;
 
+//import Acc
 public class MasterControllerActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
